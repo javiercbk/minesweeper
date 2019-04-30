6d041f49625b089b72d198fdc3ba4fb25833aee4
Javier Lecuona <javierlecuona@gmail.com>
2019-04-29 23:25:40 -0300

    added tests to gameapi

--- game/gameapi_create_test.go
+++ game/gameapi_create_test.go
@@ -4,10 +4,13 @@ import (
 	"context"
 	"net/http"
 	"testing"
+	"time"
 
 	"github.com/javiercbk/minesweeper/algebra"
 	"github.com/javiercbk/minesweeper/http/response"
 	"github.com/javiercbk/minesweeper/models"
+	"github.com/volatiletech/null"
+	"github.com/volatiletech/sqlboiler/queries/qm"
 )
 
 func TestCreateGame(t *testing.T) {
@@ -123,12 +126,13 @@ func TestCreateGame(t *testing.T) {
 	}
 }
 
-func TestApplySuccessfulRevealOperations(t *testing.T) {
+func TestApplyRevealOperations(t *testing.T) {
 	ctx := context.Background()
 	// ctxTimeout, _ := context.WithTimeout(ctx, time.Second*5)
-	api, user, _ := setUp(ctx, t, username)
+	api, user, otherUser := setUp(ctx, t, username)
 	tests := []struct {
 		game                 *models.Game
+		finished             bool
 		initialBoard         [][]int
 		operation            Operation
 		expectedBoard        [][]int
@@ -136,6 +140,136 @@ func TestApplySuccessfulRevealOperations(t *testing.T) {
 		err                  error
 	}{
 		{
+			game: &models.Game{
+				// should fail when attempting to apply an operation in a private game
+				// that was created by another user
+				CreatorID: otherUser.ID,
+				Rows:      int16(3),
+				Cols:      int16(3),
+				Mines:     int16(2),
+				Private:   true,
+			},
+			initialBoard: [][]int{
+				{-2, -9, -2},
+				{-2, -3, -3},
+				{-1, -2, -9},
+			},
+			operation: Operation{
+				ID:  1,
+				Row: 0,
+				Col: 0,
+				Op:  algebra.OpReveal,
+			},
+			err: response.HTTPError{
+				Code:    http.StatusNotFound,
+				Message: ErrGameNotExists.Error(),
+			},
+		},
+		{
+			game: &models.Game{
+				CreatorID: user.ID,
+				Rows:      int16(3),
+				Cols:      int16(3),
+				Mines:     int16(2),
+				Private:   false,
+			},
+			initialBoard: [][]int{
+				{-2, -9, -2},
+				{-2, -3, -3},
+				{-1, -2, -9},
+			},
+			operation: Operation{
+				ID:  1,
+				Row: 0,
+				Col: 0,
+				Op:  algebra.OpReveal,
+				// should fail when game id does not exist
+				GameID: -1,
+			},
+			err: response.HTTPError{
+				Code:    http.StatusNotFound,
+				Message: ErrGameNotExists.Error(),
+			},
+		},
+		{
+			game: &models.Game{
+				CreatorID: user.ID,
+				Rows:      int16(3),
+				Cols:      int16(3),
+				Mines:     int16(2),
+				Private:   false,
+			},
+			// should not allow to apply operations on finished games
+			finished: true,
+			initialBoard: [][]int{
+				{-2, -9, -2},
+				{-2, -3, -3},
+				{-1, -2, -9},
+			},
+			operation: Operation{
+				ID:  1,
+				Row: 0,
+				Col: 0,
+				Op:  algebra.OpReveal,
+			},
+			err: response.HTTPError{
+				Code:    http.StatusNotFound,
+				Message: ErrGameFinished.Error(),
+			},
+		},
+		{
+			game: &models.Game{
+				CreatorID:  user.ID,
+				Rows:       int16(3),
+				Cols:       int16(3),
+				Mines:      int16(2),
+				Private:    false,
+				FinishedAt: null.NewTime(time.Now().UTC(), true),
+			},
+			initialBoard: [][]int{
+				{-2, -9, -2},
+				{-2, -3, -3},
+				{-1, -2, -9},
+			},
+			operation: Operation{
+				ID: 1,
+				// should fail with this row overflow
+				Row: 3,
+				Col: 0,
+				Op:  algebra.OpReveal,
+			},
+			err: response.HTTPError{
+				Code:    http.StatusBadRequest,
+				Message: ErrInvalidRowCols.Error(),
+			},
+		},
+		{
+			game: &models.Game{
+				CreatorID: user.ID,
+				Rows:      int16(3),
+				Cols:      int16(3),
+				Mines:     int16(2),
+				Private:   false,
+			},
+			initialBoard: [][]int{
+				{-2, -9, -2},
+				{-2, -3, -3},
+				{-1, -2, -9},
+			},
+			operation: Operation{
+				ID:  1,
+				Row: 0,
+				// should fail with this column overflow
+				Col: 3,
+				Op:  algebra.OpReveal,
+			},
+			err: response.HTTPError{
+				Code:    http.StatusBadRequest,
+				Message: ErrInvalidRowCols.Error(),
+			},
+		},
+		{
+			// should apply the reveal operation
 			game: &models.Game{
 				CreatorID: user.ID,
 				Rows:      int16(3),
@@ -173,14 +307,61 @@ func TestApplySuccessfulRevealOperations(t *testing.T) {
 				{-1, -2, -9},
 			},
 		},
+		{
+			game: &models.Game{
+				CreatorID: otherUser.ID,
+				Rows:      int16(3),
+				Cols:      int16(3),
+				Mines:     int16(2),
+				// should allow other user to play non private game
+				Private: false,
+			},
+			initialBoard: [][]int{
+				{-2, -9, -2},
+				{-2, -3, -3},
+				{-1, -2, -9},
+			},
+			operation: Operation{
+				ID:  1,
+				Row: 0,
+				Col: 0,
+				Op:  algebra.OpReveal,
+			},
+			expectedConfirmation: OperationConfirmation{
+				Operation: Operation{
+					ID:      1,
+					Row:     0,
+					Col:     0,
+					Op:      algebra.OpReveal,
+					Applied: true,
+				},
+				Status: Status{
+					Rows: 3,
+					Cols: 3,
+				},
+			},
+			expectedBoard: [][]int{
+				{1, -9, -2},
+				{-2, -3, -3},
+				{-1, -2, -9},
+			},
+		},
 	}
 	for i, test := range tests {
 		err := api.storeGameBoard(ctx, user, test.game, test.initialBoard)
-		test.expectedConfirmation.Operation.GameID = test.game.ID
 		if err != nil {
 			t.Fatalf("test %d, failed: error creating board %v\n", i, err)
 		}
-		test.operation.GameID = test.game.ID
+		if test.finished {
+			_, err = models.Games(qm.Where("id = ?", test.game.ID)).UpdateAll(ctx, api.db, models.M{"finished_at": time.Now().UTC()})
+			if err != nil {
+				t.Fatalf("test %d, failed: error setting gam as finished board %v\n", i, err)
+			}
+		}
+		test.expectedConfirmation.Operation.GameID = test.game.ID
+		if test.operation.GameID >= 0 {
+			test.operation.GameID = test.game.ID
+		}
 		confirmation, err := api.ApplyOperation(ctx, user, test.operation)
 		if err != test.err {
 			t.Fatalf("test %d failed: expected err to be %v, but was %v\n", i, test.err, err)
